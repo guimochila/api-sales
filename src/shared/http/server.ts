e5255845b516298e77750a80dcdf4bbac7225caf
Guilherme <gui.mochila@gmail.com>
2022-01-09 14:57:00 +0100

    feature: Data validation and error handler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata'
 import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import cors from 'cors'
+import { errors } from 'celebrate'
 import routes from './routes'
 import AppError from '@shared/errors/AppError'
 import '@shared/typeorm'
@@ -12,6 +14,7 @@ app.use(express.json())
 
 app.use(routes)
 
+app.use(errors())
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
