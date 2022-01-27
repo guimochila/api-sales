6dbd060cffca2fdd8ad100f90a171f890cbbeb03
Guilherme <gui.mochila@gmail.com>
2022-01-27 08:38:07 +0100

    feature: Add Pagination

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -4,6 +4,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { errors } from 'celebrate'
+import { pagination } from 'typeorm-pagination'
 import routes from './routes'
 import AppError from '@shared/errors/AppError'
 import '@shared/typeorm'
@@ -13,7 +14,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-
+app.use(pagination)
 app.use('/static', express.static(uploadConfig.directory))
 app.use(routes)
 
