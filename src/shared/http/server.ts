8dc8e69e7b4a28e0f7ca27a1cc02431fc5901318
Guilherme <gui.mochila@gmail.com>
2022-01-27 08:50:24 +0100

    feature: Configure env variables

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -37,6 +37,8 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   })
 })
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333! 🚀')
+const port = process.env.PORT
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}! 🚀`)
 })
